1656885411f592e06dfe769fba4ef89696ee2352
microcephalus7 <microcephalus7@gmail.com>
2021-08-30 22:07:47 +0900

    Add 목표 Model 작업중

--- back/goal/models.py
+++ back/goal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Goal(models.Model):
+    pubDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now=True)
+
+    title = models.TextField(max_length=50)
+    description = models.TextField(max_length=200, null=True)
+    status = ["not yet", "doing", "done", "next time"]
+
+    startDate = models.DateTimeField(blank=True)
+    endDate = models.DateTimeField(blank=True)
